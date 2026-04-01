dc3e6ef31aa83607fe9eda11865ebed91238086d
SOPAN RAMESHWAR WARADE <144265211+sopanwarade5@users.noreply.github.com>
2026-04-01 17:49:21 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://smart-park-49ix.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
